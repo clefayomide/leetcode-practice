78b0fda0b660c4d4b4e99b2512fe26c3634216bf
victor <victorayomide32@gmail.com>
2024-09-07 20:23:12 +0100

    first commit

---
+++ running_sum_of_1d_array.js
@@ -0,0 +1,17 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+const runningSum = function (nums) {
+	const indexValue = nums[0];
+	const runningSum = [indexValue];
+	nums.splice(1).forEach((num) => {
+		const runningSumLastIndex = runningSum.length - 1;
+		const lastSum = runningSum[runningSumLastIndex];
+		const newSum = lastSum + num;
+		runningSum.push(newSum);
+	});
+	return runningSum;
+};
+
+// time complexity: o(n)
+// formulate used to arrive at the time complexity: c1 + c1 + cn + c1 + c1 +c1 + c1
